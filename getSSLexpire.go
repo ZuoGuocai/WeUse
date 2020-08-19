5be41a0e342799584275549bf1ff5e95f66c4caf
Even <zuoguocai@126.com>
2020-08-19 17:15:46 +0800

    Update getSSLexpire.go

--- getSSLexpire.go
+++ getSSLexpire.go
@@ -1,68 +1,80 @@
+/*
+  Author: zuoguocai@126.com
+Function: getSSLInfo
+ version: 1.0
+*/
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
-	"github.com/olekukonko/tablewriter"
-	"net/http"
-	"os"
-	"strconv"
-	//    "log"
-	//"strings"
+        "crypto/tls"
+        "fmt"
+        "github.com/olekukonko/tablewriter"
+        "net/http"
+        "os"
+        "strconv"
+        //"log"
+        //"strings"
+        "math"
+        "time"
 )
 
-func get_info(url string) {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
+var messages chan []string = make(chan []string, 1000)
 
-	seedUrl := url
-	resp, err := client.Get(seedUrl)
-	defer resp.Body.Close()
+func getSSLInfo(url string) (data []string) {
 
-	if err != nil {
-		fmt.Errorf(seedUrl, " 请求失败")
-		panic(err)
-	}
+        tr := &http.Transport{
+                TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+        }
+        client := &http.Client{Transport: tr}
 
-	//fmt.Println(resp.TLS.PeerCertificates[0])
-	certInfo := resp.TLS.PeerCertificates[0]
-	NotBefore := certInfo.NotBefore.Format("2006-01-02 15:04:05")
-	NotAfter := certInfo.NotAfter.Format("2006-01-02 15:04:05")
-	aa := certInfo.NotBefore
-	bb := certInfo.NotAfter
-	nn := bb.Sub(aa).Hours() / 24
-	mm := strconv.FormatFloat(nn, 'f', 2, 64)
-	//DNSNames := strings.Join(certInfo.DNSNames,"")
-	//Subject := certInfo.Subject.String()
-	//Issuer := certInfo.Issuer.String()
+        seedUrl := url
+        resp, err := client.Get(seedUrl)
+        defer resp.Body.Close()
 
-	data := [][]string{
-		[]string{"https", seedUrl, NotBefore, NotAfter, mm},
-	}
+        if err != nil {
+                fmt.Errorf(seedUrl, " 请求失败")
+                panic(err)
+        }
 
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"监控类型", "监控URL", "开始时间", "结束时间", "还有n天到期"})
+        //fmt.Println(resp.TLS.PeerCertificates[0])
+        certInfo := resp.TLS.PeerCertificates[0]
+        NotBefore := certInfo.NotBefore.Format("2006-01-02 15:04:05")
+        NotAfter := certInfo.NotAfter.Format("2006-01-02 15:04:05")
+        bb := certInfo.NotAfter
+        now := time.Now()
+        nn := math.Ceil(bb.Sub(now).Hours() / 24)
+        mm := strconv.FormatFloat(nn, 'f', 2, 64)
+        //DNSNames := strings.Join(certInfo.DNSNames,"")
+        //Subject := certInfo.Subject.String()
+        //Issuer := certInfo.Issuer.String()
 
-	for _, v := range data {
-		table.Append(v)
-	}
-	table.Render() // Send output
-
+        //data := [][]string{
+        //      []string{"https", seedUrl, NotBefore, NotAfter, mm},
+        //}
+        data = []string{"https", seedUrl, NotBefore, NotAfter, mm}
+        return
 }
+
+
+func genTable(data [][]string) {
+        table := tablewriter.NewWriter(os.Stdout)
+        table.SetHeader([]string{"监控类型", "监控URL", "开始时间", "结束时间", "还有n天到期"})
+
+        for _, v := range data {
+                table.Append(v)
+        }
+        table.Render() // Send output
+	}
 
 func main() {
-/*
-	var  myurl = [...]string{"https://youtube.com", "https://baidu.com", "https://vipkid.com.cn"}
-	for _, v := range myurl {
-		//get_info(v)
-               fmt.Println(v)
 
-	}
-*/
-        get_info("https://youtube.com")
-        get_info("https://baidu.com")
-        get_info("https://google.com")
-
+        var myurl = [...]string{"https://youtube.com", "https://google.com", "https://www.hao123.com","https://www.baidu.com"}
+        var tableList [][]string
+        for _, v := range myurl {
+                aa := getSSLInfo(v)
+                tableList = append(tableList, aa)
+        }
+        genTable(tableList)
 }
+
+	
